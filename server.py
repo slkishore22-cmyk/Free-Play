c90b67a6275ebe74d00e521d491758d68cfcbafe
Kishore <slkishore22@gmail.com>
2026-06-06 23:25:20 +0530

    Enable CORS on playlist scraper server

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import requests
 import re
 
 app = Flask(__name__)
+CORS(app)
 
 def extract_playlist_id(url):
     match = re.search(r'playlist/([a-zA-Z0-9]+)', url)
